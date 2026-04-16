4966a19848bee96b7b177f7e05857aa323d38893
Ugandaboi <01199214@pw.edu.pl>
2026-04-16 13:37:46 +0200

    parts1-3

--- PolishTask.c
+++ PolishTask.c
@@ -29,6 +29,14 @@
         exit(EXIT_FAILURE);                             \
     } while (0)
 
+
+typedef struct {
+    pthread_barrier_t barrier;
+    pthread_mutex_t keys_mutexes[MAX_KEYBOARDS * MAX_KEYS];
+    double keys_state[MAX_KEYBOARDS * MAX_KEYS];
+} SharedData;
+
+
 void usage(char* program_name)
 {
     fprintf(stderr, "Usage: \n");
@@ -78,7 +86,7 @@ void print_keyboards_state(double* keyboards, int m, int k)
     }
 }
 
-void child_work(int m,int k){
+void child_work(int m,int k, SharedData *shared){
     srand((unsigned)(time(NULL) ^ (unsigned long)getpid()));
     sem_t **sems = malloc(sizeof(sem_t) * m);
     if(sems == NULL)
@@ -92,20 +100,42 @@ void child_work(int m,int k){
         free(name);
     }
     for(int j = 0; j < 10;j++){
-        int index = rand() % m;
-        sem_wait(sems[index]);
-        printf("PID<%d>: cleaning keyboard <%d> \n",(pid_t)getpid(),(index + 1));
+        // keyboard  and  key indices 
+        int keyboard_index = rand() % m;
+        int key_index = rand() % k;
+
+        // index for  mutex 
+        int total_key_index = keyboard_index * k + key_index;
+
+        sem_wait(sems[keyboard_index]);
+        pthread_mutex_lock(&shared->keys_mutexes[total_key_index]);
+        
+        printf("PID<%d>: cleaning keyboard <%d> , key: %d \n",(pid_t)getpid(),(keyboard_index + 1), key_index);
         ms_sleep(300);
-        sem_post(sems[index]);
+
+        // then divide the value of the corresponding field by 3
+        shared->keys_state[total_key_index] /= 3.0;
+
+        pthread_mutex_unlock(&shared->keys_mutexes[total_key_index]);
+        sem_post(sems[keyboard_index]);
     }
+
+    
     for(int j = 0; j<m;j++){
         sem_close(sems[j]);
     }
     free(sems);
+
+    if ((rand() % 100) < 3) {
+        fprintf(stderr, "[worker %d] simulating crash (abort)\n", getpid());
+        abort();
+    }
+
+    munmap(shared, sizeof(SharedData));
     exit(EXIT_SUCCESS);
 }
 
-int create_workers(pid_t *workers,int n, int m, int k){
+    int create_workers(pid_t *workers,int n, int m, int k, SharedData *shared){
     for(int i = 0; i < n;i++){
         workers[i] = fork();
         if(workers[i] == -1){
@@ -113,7 +143,9 @@ int create_workers(pid_t *workers,int n, int m, int k){
             return -1;
         }
         if(workers[i] == 0){
-            child_work(m,k);
+            pthread_barrier_wait(&shared->barrier);
+            child_work(m,k,shared);
+            exit(EXIT_SUCCESS);
         }
     }
     return 0;
@@ -175,10 +207,11 @@ int parse_args(int argc, char **argv, int *n, int *m, int *k)
 
 int main(int argc, char** argv) 
 { 
+    remove_semafores(MAX_KEYBOARDS);
+
     int n,m,k;
     if(parse_args(argc, argv, &n,&m,&k) == -1)
         exit(EXIT_FAILURE);
-    remove_semafores(n);
 
     pid_t *workers = (pid_t*)malloc(sizeof(pid_t) * n);
     if(workers == NULL){
@@ -186,18 +219,92 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-    if(create_workers(workers, n, m,k) == -1)
+    // part 2 
+    // pthread_barrier_t *barrier = mmap(NULL,sizeof(pthread_barrier_t),PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    // if(barrier == MAP_FAILED)
+    //     ERR(mmap);
+
+    // part 2 
+    // pthread_barrierattr_t attr;
+    // pthread_barrierattr_init(&attr);
+    // pthread_barrierattr_setpshared(&attr,PTHREAD_PROCESS_SHARED);
+    // if(pthread_barrier_init(barrier, &attr, n+1)!=0)
+    //     ERR("couldn't initialize barrier");
+    // pthread_barrierattr_destroy(&attr);
+
+    // if(create_workers(workers, n, m,k, barrier) == -1)
+    //     ERR("couldn't create processes");
+    
+    // rodzic  tworzy i spokojnie  tworzy 
+    // chcemy size dla wiec liczymi ile czego jest  
+    //use a separate mutex for each key.
+    size_t rozmiar = sizeof(SharedData);
+
+    shm_unlink(SHARED_MEM_NAME);
+
+    int fd = shm_open(SHARED_MEM_NAME, O_CREAT | O_RDWR, 0644);
+    if(ftruncate(fd, rozmiar) == -1)
+        ERR("ftruncate");
+
+
+    SharedData* shared = mmap(NULL, rozmiar, PROT_READ | PROT_WRITE , MAP_SHARED , fd ,0);
+    if(shared == NULL)
+        ERR("mmap");
+    close(fd);
+
+    // barrier  init  for  part3 
+    pthread_barrierattr_t attr;
+    pthread_barrierattr_init(&attr);
+    pthread_barrierattr_setpshared(&attr,PTHREAD_PROCESS_SHARED);
+    if(pthread_barrier_init(&shared->barrier, &attr, n+1)!=0)
+        ERR("couldn't initialize barrier");
+    pthread_barrierattr_destroy(&attr);
+
+    // mutex  init  for  part  3  
+
+    pthread_mutexattr_t m_attr;
+    pthread_mutexattr_init(&m_attr);
+    pthread_mutexattr_setpshared(&m_attr, PTHREAD_PROCESS_SHARED);
+
+    for(int i = 0; i < m * k; i++){
+        shared->keys_state[i] = 1.0;
+        pthread_mutex_init(&shared->keys_mutexes[i],&m_attr);
+    }
+    pthread_mutexattr_destroy(&m_attr);
+
+
+
+    if(create_workers(workers, n, m,k, shared) == -1)
         ERR("couldn't create processes");
+
+
+    ms_sleep(500);
+    pthread_barrier_wait(&shared->barrier);
 
     int all_ok = wait_for_child(n, workers);
 
     if (!all_ok) {
-        printf("\n Failed: worker crashed \n");
+        printf("Computation failed!!!\n");
     } else {
-        remove_semafores(m);
+        printf("Cleaning finished! Final keyboards state:\n");
+        print_keyboards_state(shared->keys_state, m, k);
     }
-    printf("Cleaning finished! \n");
     
+    
+    //printf("Cleaning finished! \n");
+    
+    remove_semafores(m);    
+
+    pthread_barrier_destroy(&shared->barrier);
+    for(int i = 0 ; i < m*k ; i++)
+        pthread_mutex_destroy(&shared->keys_mutexes[i]);
+    
+    // zazmiana  teraz  inne mapowanie  juz  nie  
+    //munmap(&shared->barrier, sizeof(pthread_barrier_t)); 
+
     free(workers);
+    // tylko 
+    munmap(shared, sizeof(SharedData));
+    shm_unlink(SHARED_MEM_NAME);
     exit(EXIT_SUCCESS);
 }
